fb736bc485412c73641f7744d88fdfb9690e4a91
Jose Murillo <jooseemurillo@gmail.com>
2020-01-14 10:35:18 -0300

    connect mongo local

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import { connect } from 'mongoose';
 
 const app = express();
 
 // Routes params são usados quando queremos usar: DELETE ou PUT 
 // Body é usado no POST ou PUT para o envio dos dados do corpo da aplicação
+
+connect('mongodb://localhost/week10', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.json());
 
