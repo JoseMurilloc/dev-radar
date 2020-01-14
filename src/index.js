40e5af137a92b97d1cdef97f78ee444516ce9a25
Jose Murillo <jooseemurillo@gmail.com>
2020-01-14 10:11:15 -0300

    req.body

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import express from 'express';
 const app = express();
 
 // Routes params são usados quando queremos usar: DELETE ou PUT 
+// Body é usado no POST ou PUT para o envio dos dados do corpo da aplicação
 
-app.delete('/users/:id', (req, res) => {
-  console.log(req.params.id);
-  return res.json({ dev: true });
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+  const user = req.body;
+  return res.json(user);
 });
 
 app.listen(3333);
