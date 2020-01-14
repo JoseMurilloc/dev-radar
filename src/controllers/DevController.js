1f1e5bbd6af834298323ba881a1d839714b64ea7
Jose Murillo <jooseemurillo@gmail.com>
2020-01-14 12:11:18 -0300

    add busca por todos devs

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -3,9 +3,21 @@ import Dev from '../models/Dev';
 
 
 class DevController {
+
+  async index(request, response) {
+    const devs = await Dev.find();
+    return  response.json(devs);
+  }
+
   async create(request, response) {
     const { github_username, techs, latitude, longitude } = request.body;
-  
+    
+    const existDev = await Dev.findOne({ github_username });
+
+    if(existDev) {
+      return response.json(existDev);
+    }
+
     const techsArray = techs.split(', ').map(tech => tech.trim());
   
     const { data } = await axios.get(`https://api.github.com/users/${github_username}`);
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import DevController from './controllers/DevController';
 
 const routes = Router();
 
+routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.create);
 
 export default routes;
